139dba4f609160493a0d052a3c094a734149cd06
sbleidner <sascha@codementors.de>
2024-02-25 01:22:26 +0100

    feat: render issue number in first line if git commit is used with an editor  (GL-8)

--- tests/test_add_issue_number.py
+++ tests/test_add_issue_number.py
@@ -15,6 +15,12 @@ def test_add_issue_number_between_description_and_body():
     assert new_content == "chore: initial commit (PRE-10)\n\nthis is a body"
 
 
+def test_add_issue_number_in_first_line_if_commit_message_contains_git_defaults():
+    new_content = add_issue_number("# Please enter the commit message\n\n", "PRE-10")
+
+    assert new_content == "(PRE-10) # Please enter the commit message\n\n"
+
+
 def test_add_issue_number_is_not_added_if_its_already_there():
     new_content = add_issue_number("chore: initial commit (PRE-10)", "PRE-10")
 
